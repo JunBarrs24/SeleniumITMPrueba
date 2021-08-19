662435c0492940f657fa4dcc1389137bbde6c1f9
Juan Barrera <barrera.yo@gmail.com>
2021-08-19 18:42:00 -0500

    codigo base

---
+++ src/test/java/Test/TestJ1.java
@@ -0,0 +1,36 @@
+package Test;
+
+import org.junit.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class TestJ1 {
+
+    private WebDriver driver;
+
+    @BeforeClass
+    public static void setupWebdriverChromeDriver() {
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/main/resources/chromedriver.exe");
+    }
+
+    @Before
+    public void setup() {
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void verifyGitHubTitle() {
+        driver.get("http://dsc.itmorelia.edu.mx/");
+        assertThat(driver.getTitle(), containsString("Sistemas y Computación | ITM"));
+    }
+}
